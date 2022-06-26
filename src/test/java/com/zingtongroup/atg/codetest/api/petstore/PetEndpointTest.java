5242723ac8d0a1f5cfd6892e53a26a859d0cad03
Eric Thelin <me@ericthelin.se>
2022-06-26 12:20:12 +0200

    Test that the pet is preserved between requests

--- src/test/java/com/zingtongroup/atg/codetest/api/petstore/PetEndpointTest.java
+++ src/test/java/com/zingtongroup/atg/codetest/api/petstore/PetEndpointTest.java
@@ -27,6 +27,30 @@ public class PetEndpointTest {
 				.body("id", notNullValue());
 	}
 
+	@Test
+	void remembersPet() {
+		// Given
+		Long id = anExistingPet(Map.of("name", "my-pet"));
+
+		// When
+		Response response = when().get(String.valueOf(id));
+
+		// Then
+		response.then().statusCode(200)
+				.body("name", is("my-pet"))
+				.body("id", is(id));
+	}
+
+	private Long anExistingPet(Map<String, String> data) {
+		return given()
+				.body(data)
+				.when().post().then().statusCode(200).extract().path("id");
+	}
+
+	private RequestSpecification when() {
+		return given().when();
+	}
+
 	private RequestSpecification given() {
 		RequestSpecification result = RestAssured.given()
 				.baseUri("https://petstore.swagger.io/v2/pet")
