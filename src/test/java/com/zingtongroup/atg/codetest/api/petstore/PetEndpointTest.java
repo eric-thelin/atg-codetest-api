aa167ae794dd2967cad2dbb8fc7d9e4cecfd6f8b
Eric Thelin <me@ericthelin.se>
2022-06-26 12:20:12 +0200

    Test update by put

--- src/test/java/com/zingtongroup/atg/codetest/api/petstore/PetEndpointTest.java
+++ src/test/java/com/zingtongroup/atg/codetest/api/petstore/PetEndpointTest.java
@@ -99,7 +99,7 @@ public class PetEndpointTest {
 	}
 
 	@Test
-	void acceptsMinimalUpdate() {
+	void acceptsMinimalUpdateByPut() {
 		// Given
 		Long id = anExistingPet(Map.of(
 				"name", "my-pet",
@@ -127,7 +127,7 @@ public class PetEndpointTest {
 	}
 
 	@Test
-	void acceptsFullUpdate() {
+	void acceptsFullUpdateByPut() {
 		// Given
 		Long id = anExistingPet(Map.of(
 				"name", "my-pet",
@@ -164,6 +164,60 @@ public class PetEndpointTest {
 						Map.of("id", 3, "name", "t3"),
 						Map.of("id", 4, "name", "t4")
 				)));
+	}
+
+	@Test
+	void acceptsMinimalUpdateByPost() {
+		// Given
+		Long id = anExistingPet(Map.of(
+				"name", "my-pet",
+				"status", "pending",
+				"photoUrls", List.of("p1"),
+				"tags", List.of(Map.of("id", 1, "name", "t1"))
+		));
+
+		RequestSpecification request = given().body(Map.of("id", id));
+
+		// When
+		Response response = request.when().post();
+
+		// Then
+		response.then().statusCode(200)
+				.body("id", is(id))
+				.body("name", is(nullValue()))
+				.body("status", is(nullValue()))
+				.body("photoUrls", is(empty()))
+				.body("tags", is(empty()));
+	}
+
+	@Test
+	void acceptsFullUpdateByPost() {
+		// Given
+		Long id = anExistingPet(Map.of(
+				"name", "my-pet",
+				"status", "pending",
+				"photoUrls", List.of("p1"),
+				"tags", List.of(Map.of("id", 1, "name", "t1"))
+		));
+
+		RequestSpecification request = given().body(Map.of(
+				"id", id,
+				"name", "new-name",
+				"status", "sold",
+				"photoUrls", List.of("p2"),
+				"tags", List.of(Map.of("id", 2, "name", "t2"))
+		));
+
+		// When
+		Response response = request.when().post();
+
+		// Then
+		response.then().statusCode(200)
+				.body("id", is(id))
+				.body("name", is("new-name"))
+				.body("status", is("sold"))
+				.body("photoUrls", is(List.of("p2")))
+				.body("tags", is(List.of(Map.of("id", 2, "name", "t2"))));
 	}
 
 	@ParameterizedTest
