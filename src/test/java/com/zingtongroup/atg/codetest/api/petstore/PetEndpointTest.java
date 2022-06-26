da404172788fc0ec0be0c8c42fdab388a846ee46
Eric Thelin <me@ericthelin.se>
2022-06-26 12:20:12 +0200

    Test update pet by PUT

--- src/test/java/com/zingtongroup/atg/codetest/api/petstore/PetEndpointTest.java
+++ src/test/java/com/zingtongroup/atg/codetest/api/petstore/PetEndpointTest.java
@@ -94,6 +94,74 @@ public class PetEndpointTest {
 				)));
 	}
 
+	@Test
+	void acceptsMinimalUpdate() {
+		// Given
+		Long id = anExistingPet(Map.of(
+				"name", "my-pet",
+				"status", "pending",
+				"photoUrls", List.of("p1", "p2", "p3"),
+				"tags", List.of(
+						Map.of("id", 1, "name", "t1"),
+						Map.of("id", 2, "name", "t2"),
+						Map.of("id", 3, "name", "t3")
+				)
+		));
+
+		RequestSpecification request = given().body(Map.of("id", id));
+
+		// When
+		Response response = request.when().put();
+
+		// Then
+		response.then().statusCode(200)
+				.body("id", is(id))
+				.body("name", is(nullValue()))
+				.body("status", is(nullValue()))
+				.body("photoUrls", is(empty()))
+				.body("tags", is(empty()));
+	}
+
+	@Test
+	void acceptsFullUpdate() {
+		// Given
+		Long id = anExistingPet(Map.of(
+				"name", "my-pet",
+				"status", "pending",
+				"photoUrls", List.of("p1", "p2", "p3"),
+				"tags", List.of(
+						Map.of("id", 1, "name", "t1"),
+						Map.of("id", 2, "name", "t2"),
+						Map.of("id", 3, "name", "t3")
+				)
+		));
+
+		RequestSpecification request = given().body(Map.of(
+				"id", id,
+				"name", "new-name",
+				"status", "sold",
+				"photoUrls", List.of("p3", "p4"),
+				"tags", List.of(
+						Map.of("id", 3, "name", "t3"),
+						Map.of("id", 4, "name", "t4")
+				)
+		));
+
+		// When
+		Response response = request.when().put();
+
+		// Then
+		response.then().statusCode(200)
+				.body("id", is(id))
+				.body("name", is("new-name"))
+				.body("status", is("sold"))
+				.body("photoUrls", is(List.of("p3", "p4")))
+				.body("tags", is(List.of(
+						Map.of("id", 3, "name", "t3"),
+						Map.of("id", 4, "name", "t4")
+				)));
+	}
+
 	private Long anExistingPet(Map<String, Object> data) {
 		return given()
 				.body(data)
