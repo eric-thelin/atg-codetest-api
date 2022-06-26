2db493f393b2b4d96ad6f13440b295cc132e579e
Eric Thelin <me@ericthelin.se>
2022-06-26 12:20:12 +0200

    Test creating pet with minimum and maximum anmount of data

--- src/test/java/com/zingtongroup/atg/codetest/api/petstore/PetEndpointTest.java
+++ src/test/java/com/zingtongroup/atg/codetest/api/petstore/PetEndpointTest.java
@@ -5,15 +5,18 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 
 public class PetEndpointTest {
 
 	@Test
-	void acceptsNewPet() {
+	void acceptsMinimalPet() {
 		// Given
 		RequestSpecification request = given()
 				.body(Map.of("name", "my-pet"));
@@ -23,25 +26,75 @@ public class PetEndpointTest {
 
 		// Then
 		response.then().statusCode(200)
+				.body("id", notNullValue())
 				.body("name", is("my-pet"))
-				.body("id", notNullValue());
+				.body("status", is(nullValue()))
+				.body("photoUrls", is(empty()))
+				.body("tags", is(empty()));
+	}
+
+	@Test
+	void acceptsFullPet() {
+		// Given
+		RequestSpecification request = given()
+				.body(Map.of(
+						"name", "my-pet",
+						"status", "pending",
+						"photoUrls", List.of("p1", "p2", "p3"),
+						"tags", List.of(
+								Map.of("id", 1, "name", "t1"),
+								Map.of("id", 2, "name", "t2"),
+								Map.of("id", 3, "name", "t3")
+						)
+				));
+
+		// When
+		Response response = request.when().post();
+
+		// Then
+		response.then().statusCode(200)
+				.body("id", notNullValue())
+				.body("name", is("my-pet"))
+				.body("status", is("pending"))
+				.body("photoUrls", is(List.of("p1", "p2", "p3")))
+				.body("tags", is(List.of(
+						Map.of("id", 1, "name", "t1"),
+						Map.of("id", 2, "name", "t2"),
+						Map.of("id", 3, "name", "t3")
+				)));
 	}
 
 	@Test
 	void remembersPet() {
 		// Given
-		Long id = anExistingPet(Map.of("name", "my-pet"));
+		Long id = anExistingPet(Map.of(
+				"name", "my-pet",
+				"status", "pending",
+				"photoUrls", List.of("p1", "p2", "p3"),
+				"tags", List.of(
+						Map.of("id", 1, "name", "t1"),
+						Map.of("id", 2, "name", "t2"),
+						Map.of("id", 3, "name", "t3")
+				)
+		));
 
 		// When
 		Response response = when().get(String.valueOf(id));
 
 		// Then
 		response.then().statusCode(200)
+				.body("id", is(id))
 				.body("name", is("my-pet"))
-				.body("id", is(id));
+				.body("status", is("pending"))
+				.body("photoUrls", is(List.of("p1", "p2", "p3")))
+				.body("tags", is(List.of(
+						Map.of("id", 1, "name", "t1"),
+						Map.of("id", 2, "name", "t2"),
+						Map.of("id", 3, "name", "t3")
+				)));
 	}
 
-	private Long anExistingPet(Map<String, String> data) {
+	private Long anExistingPet(Map<String, Object> data) {
 		return given()
 				.body(data)
 				.when().post().then().statusCode(200).extract().path("id");
